0cc86540c019ea0bb692fa18727fdfa653185ae0
David Fox <dsf@seereason.com>
2016-06-21 12:00:19 -0700

    Add code to print timing of largeDocRender test

--- tests/UnitLargeDoc.hs
+++ tests/UnitLargeDoc.hs
@@ -4,13 +4,22 @@ import Text.PrettyPrint.HughesPJ
 
 import Control.DeepSeq
 import Control.Exception
+import Data.Time (diffUTCTime, getCurrentTime, NominalDiffTime)
 
 testLargeDoc :: IO ()
 testLargeDoc = do
   putStrLn "Testing large doc..."
-  evaluate largeDocRender
+  (r, t) <- timeTask $ evaluate largeDocRender
+  putStrLn $ "Elapsed: " ++ show t
   return ()
 
 largeDocRender :: String
 largeDocRender = force $ render $ vcat $ replicate 10000000 $ text "Hello"
 
+-- | Run a task and return the elapsed time along with its result.
+timeTask :: IO a -> IO (a, NominalDiffTime)
+timeTask x =
+    do start <- getCurrentTime
+       result <- x >>= evaluate
+       finish <- getCurrentTime
+       return (result, diffUTCTime finish start)
